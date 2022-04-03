adf9054dcb21d7f92a072636c692c16147701552
Luba Kassurova <kassurova@icloud.com>
2022-04-03 17:24:04 -0400

    commit tree 6d3d93fb6302feda6a80cf752c5f871a178ea81f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(student_name, grade)
+
+        roster[grade] ||= []
+    #    if !roster[grade] 
+    #     roster[grade] = []
+    #    else 
+       roster[grade] << student_name
+     end
+
+    def grade(number)
+        roster[number]
+
+    end
+
+    def sort 
+        sorted = {}
+        roster.each do |grade,student_name|
+       sorted[grade] = student_name.sort
+        end
+         sorted
+    end
+      
+    
+end
+# def sort
+# sorted = {}
+# roster.each do |grade, students|
+#   sorted[grade] = students.sort
+# end
+# sorted
+# end
